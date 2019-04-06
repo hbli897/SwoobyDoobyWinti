0d49feb2bf1ebcbed8e879376b4ecd848535a843
Samuel dobler <markus.3000@hispeed.ch>
2019-04-06 07:41:04 +0100

    a lot of new stuff

--- Backup/Backup_1/script.js
+++ Backup/Backup_1/script.js
@@ -1,88 +1,8 @@
-
+const Date_input = document.getElementById("Inputdate");
+const Discription_input = document.getElementById("Inputdisc");
 const Output = document.getElementById("Output");
-//var Start_Notifications = ['Einkaufen', '10.04.2019', 'Aufräumen', '8.04.2019'];
-//var Dataarray_1 = [Date_input.value, Discription_input.value];
-var Date_inputv;
-var Discription_inputv; 
-
-class Store_Data {
-    constructor(Name, Output_id) {
-        this.Name = Name;
-
-        this.Output_id = Output_id;
-    }
-    
-    
-    Upload_data(data1, data2) {
-        console.log(this.Name);
-        const Data1 = data1;
-        const Data2 = data2;
-        const MyName = this.Name;
-        var retrievedObject = localStorage.getItem(this.Name);
-        var Old_Dataarray = JSON.parse(retrievedObject);
-        if (Data1 && Data2) {
-            this.Name = [Data1, Data2];
-            console.log(this.Name);
-            if (Old_Dataarray) {
-                var new_Dataarray = Old_Dataarray.concat(this.Name);
-            }
-            else {
-                var new_Dataarray = this.Name;
-            }
-            console.log(new_Dataarray);
-            localStorage.setItem(MyName, JSON.stringify(new_Dataarray));
-             location.reload();
-            console.log(JSON.stringify(new_Dataarray));
-            console.log("OK2");
-        }
-        
-        
-    }
-    write_data() {
-        var retrievedObject = localStorage.getItem(this.Name);
-        var Old_Notifications = JSON.parse(retrievedObject);
-        if (Old_Notifications) {
-            for (let i = 0; i < Old_Notifications.length; i += 2) {
-                document.getElementById(this.Output_id).innerHTML += `${Old_Notifications[i + 1]}: ${Old_Notifications[i]} <button type="button" id="Input_BTN_erledigt" onclick="Aufgaben.clear_Element(${i})">Erledigt</button><br \>`;
-            }
-        }
-    }
-    clear_Element(num) {
-        var retrievedObject = localStorage.getItem(this.Name);
-        var Old_Notifications = JSON.parse(retrievedObject);
-        for (let i = num; i < Old_Notifications.length; i += 2) {
-            Old_Notifications[i] = Old_Notifications[i + 2];
-            Old_Notifications[i + 1] = Old_Notifications[i + 3];
-        }
-        Old_Notifications.pop();
-        Old_Notifications.pop();
-        localStorage.setItem(this.Name, JSON.stringify(Old_Notifications));
-        location.reload();
-    }
-
-
-}
-let Aufgaben = new Store_Data('Aufgaben' , 'Output_Aufgaben');
-
-function Upload_Data(input_id) {
-    Date_inputv = document.getElementById("Inputdate").value;
-    Discription_inputv = document.getElementById("Inputdisc").value;
-
-    switch (input_id) {
-        case 'Input_BTN':
-            console.log("OK1");
-            console.log(Date_inputv + " : " + Discription_inputv);
-            
-            Aufgaben.Upload_data(Date_inputv, Discription_inputv);
-            break;
-    }
-}
-
-function write_all() {
-    Aufgaben.write_data();
-}
-write_all();
-/*
+var Start_Notifications = ['Einkaufen', '10.04.2019', 'Aufräumen', '8.04.2019'];
+write();
 function Upload_data() {
 
     const Date = Date_input.value;
@@ -107,7 +27,7 @@ function Upload_data() {
 
 
 
-}*//*
+}
 function write() {
     var retrievedObject = localStorage.getItem('Notifications');
     var Old_Notifications = JSON.parse(retrievedObject);
@@ -116,7 +36,7 @@ function write() {
             document.getElementById("Output").innerHTML += `${Old_Notifications[i + 1]}: ${Old_Notifications[i]} <button type="button" id="Input_BTN_erledigt" onclick="clear_element(${i})">Erledigt</button><br \>`;
         }
     }
-}*//*
+}
 function clear_element(num) {
     var retrievedObject = localStorage.getItem('Notifications');
     var Old_Notifications = JSON.parse(retrievedObject);
@@ -129,7 +49,7 @@ function clear_element(num) {
     localStorage.setItem('Notifications', JSON.stringify(Old_Notifications));
     location.reload();
 }
-*/
+
 
 function Notifications_Sort_By_Date() {
     var retrievedObject = localStorage.getItem('Notifications');
