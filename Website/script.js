373c3fae93cfc969e81438e49e9a1f9c7b8085f9
Samuel dobler <markus.3000@hispeed.ch>
2019-04-06 03:24:36 +0100

    upload

---
+++ Website/script.js
@@ -0,0 +1,47 @@
+
+
+var Start_Notifications = { 'Einkaufen': '10.04.2019', 'Aufräumen': '8.04.2019' };
+
+
+
+/*var test = new XMLHttpRequest();
+var url = "Database.json";
+test.open("GET", url, true);
+test.setRequestHeader("content-type", "application/json");
+test.onreadystatechange = function () {
+    if (test.readyState == 4 && test.status == 200) {
+        var jcontent = test.responseText;
+        console.log(jcontent);
+    }
+}
+test.send(null);
+
+function Init() {
+    var Data = {
+        "one": [15, 4.5],
+        "two": [34, 3.3],
+        "three": [67, 5.0],
+        "four": [32, 4.1]
+    };
+   
+    text = localStorage.getItem("Database");
+    obj = JSON.parse(text);
+    document.getElementById("demo").innerHTML = obj.name;*/
+
+    //var Database = JSON.stringify(Data);
+   // localStorage.setItem("Database.json", Database);
+
+    /*
+    var blob = new Blob([Database], { type: "application/json" });
+    var url = URL.createObjectURL(blob);
+
+    var a = document.createElement('a');
+    a.download = "backup.json";
+    a.href = url;
+    a.textContent = "Download backup.json";
+   // var fs = require('fs');
+   // JSON_File.writeFile("Database.json", Database);
+    *//*
+    console.log("function clicked");
+}*/
+
